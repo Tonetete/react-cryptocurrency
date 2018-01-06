4d2a8f5f898d1b86e6586eb14d0b90e62842b1aa
Tone <toneterivas@gmail.com>
2018-01-06 20:27:24 +0100

    Reducers changes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import totalBenefit from './reducer_total_benefit'
+import authReducer from './reducer_auth'
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
-  totalBenefit
+  totalBenefit,
+  auth: authReducer,
+  form: formReducer
 })
 
 export default rootReducer
