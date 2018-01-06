5489499a6e7317fe526d33c821013c36991933bc
Tone <toneterivas@gmail.com>
2018-01-06 20:26:22 +0100

    Adding coin controller

---
+++ server/controllers/coins.js
@@ -0,0 +1,18 @@
+const CoinsUser = require('../models/coinsUser');
+// import Cookies from 'js-cookie'
+
+// function checkTokenUser () {
+//     const token = Cookies.get('token')
+
+//     if (token) {
+//     // Update application state. User has token and is probably authenticated
+//     store.dispatch({ type: AUTH_USER })
+//     }
+// }
+
+
+exports.getCoinsUser = (req, res, next) => {
+    // const email = req.body.email;
+    // const password = 
+    console.log('req', req.cookies )
+} 
