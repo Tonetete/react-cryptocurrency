2436a3ebc0f4b226f2c3229ef09494b7959bb794
Tone <toneterivas@gmail.com>
2018-01-06 20:28:12 +0100

    Creating composed component require authentication

---
+++ client/src/components/auth/require-auth.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+export default function (ComposedComponent) {
+  class Authentication extends Component {
+        static contextTypes = {
+          router: PropTypes.object
+        }
+
+        componentWillMount () {
+          if (!this.props.authenticated) {
+            this.context.router.history.push('/login')
+          }
+        }
+
+        componentWillUpdate (nextProps) {
+          if (!nextProps.authenticated) {
+            this.context.router.history.push('/login')
+          }
+        }
+
+        render () {
+          return <ComposedComponent {...this.props} />
+        }
+  }
+
+  function mapStateToProps (state) {
+    return { authenticated: state.auth.authenticated }
+  }
+
+  return connect(mapStateToProps)(Authentication)
+}
