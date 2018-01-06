0666e09b372aa8e2c9bf0959c57455b8dea7d60d
Tone <toneterivas@gmail.com>
2018-01-06 20:27:10 +0100

    Implementing app web routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,17 +5,20 @@ import App from './App'
 // import NotFoundPage from './components/pages/not-found-page';
 // import HomePage from './components/pages/home-page';
 // import Register from './components/auth/register';
-// import Login from './components/auth/login';
+import Login from './components/auth/login'
 // import Dashboard from './components/dashboard';
-// import RequireAuth from './components/auth/require-auth';
+import requireAuth from './components/auth/require-auth'
 
-export default (
-  <Route path='/' component={App}>
-    {/*
-      <Route path="register" component={Register} />
+/*
+    <Route path="register" component={Register} />
     <Route path="login" component={Login} />
     <Route path="dashboard" component={Dashboard} />
     <Route path="*" component={NotFoundPage} />
-    */}
-  </Route>
+    */
+
+export default (
+  <div>
+    <Route path='/home' component={requireAuth(App)} />
+    <Route path='/login' component={Login} />
+  </div>
 )
