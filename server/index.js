dfff119a05e929c90bc8ad994ab0aa0c1f963836
Tone <toneterivas@gmail.com>
2018-01-07 23:49:52 +0100

    Discarding OPTIONS methods to allow authorization headers for token

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ mongoose.connect(config.database);
 const server = app.listen(config.port);
 console.log('Your server is running on port ', config.port, ' .');
 
+app.set('superSecret', config.secret); // secret variable
 // Setting up basic middleware for all Express requests
 app.use(logger('dev')); // Log requests to API using morgan
 // parse urlencoded bodies to JSON 
@@ -25,10 +26,19 @@ app.use(cookieParser());
 // Enable CORS from client-side
 app.use(function(req, res, next) {  
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
   res.header("Access-Control-Allow-Credentials", "true");
-  next();
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
+  //move on
+    next();
+  }
+  // next();
 });
 
 router(app);
