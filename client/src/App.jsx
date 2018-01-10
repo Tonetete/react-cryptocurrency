1bf76e6c76cf04b01ffb5cf5dd81df489d6f61a8
Tone <toneterivas@gmail.com>
2018-01-10 22:39:26 +0100

    Playing around with modals

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { Component } from 'react'
 import { connect } from 'react-redux'
 import CoinList from './components/CoinList/coin_list'
+import Register from './components/auth/register'
 import { logoutUser } from './actions/index'
+import { Button } from 'semantic-ui-react'
 import './App.css'
 
 class App extends Component {
@@ -19,12 +21,15 @@ class App extends Component {
       totalBenefit = <h3>Total benefit {this.props.totalBenefitUSD}$ ({this.props.totalBenefitEUR}€)</h3>
     }
 
+    const buttonRegister = <Button>Multiple Modals</Button>
+
     return (
       <div className='App'>
         <h1>Welcome to {this.state.name}</h1>
         <CoinList />
         {totalBenefit}
         <a onClick={() => this.props.logoutUser()}>Logout User</a>
+        <Register buttonRegister={buttonRegister} />
       </div>
     )
   }
--- client/src/components/auth/register.jsx
+++ client/src/components/auth/register.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
-import { Field, reduxForm } from 'redux-form'
+import React, { Component } from 'react'
+import { Button, Header, Form, Modal, Message } from 'semantic-ui-react'
+import { reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 
 const form = reduxForm({
@@ -8,12 +8,12 @@ const form = reduxForm({
   validate
 })
 
-const renderField = field => (
-  <div>
-    <input className='form-control' {...field.input} />
-    { field.touched && field.error && <div className='error'>{field.error}</div>}
-  </div>
-)
+// const renderField = field => (
+//   <div>
+//     <input className='form-control' {...field.input} />
+//     { field.touched && field.error && <div className='error'>{field.error}</div>}
+//   </div>
+// )
 
 function validate (formProps) {
   const errors = {}
@@ -37,6 +37,11 @@ function validate (formProps) {
 }
 
 class Register extends Component {
+  constructor (props) {
+    super(props)
+    this.buttonRegister = props.buttonRegister
+  }
+
   handleFormSubmit (formProps) {
     this.props.registerUser(formProps)
   }
@@ -45,39 +50,63 @@ class Register extends Component {
     if (this.props.errorMessage) {
       return (
         <div>
-          <span><strong>Error!</strong> {this.props.errorMessage)}</span>
+          <span><strong>Error!</strong> {this.props.errorMessage}</span>
         </div>
       )
     }
   }
 
   render () {
-    const { handleSubmit } = this.props
+    // const { handleSubmit } = this.props
 
     return (
-      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-      {this.renderAlert()}
-      <div className='row'>
-        <div className='col-md-6'>
-          <label>First Name</label>
-          <Field name='firstName' className='form-control' component={renderField} type='text' />
-        </div>
-        <div className='col-md-6'>
-          <label>Last Name</label>
-          <Field name='laststName' className='form-control' component={renderField} type='text' />
-        </div>
-        <div className='col-md-6'>
-          <label>User/Email</label>
-          <Field name='email' className='form-control' component={renderField} type='text' />
-        </div>
-        <div className='col-md-6'>
-          <label>Password</label>
-          <Field name='password' className='form-control' component={renderField} type='text' />
-        </div>
-        <button type="submit" className="btn btn-primary">Register</button>
+      <div>
+        <Modal trigger={this.props.buttonRegister}>
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content>
+            <Modal.Description>
+              <Header>Default Profile Image</Header>
+              <p>We've found the following gravatar image associated with your e-mail address.</p>
+              <p>Is it okay to use this photo?</p>
+            </Modal.Description>
+            <Form error>
+              <Form.Input label='Email' placeholder='joe@schmoe.com' />
+              <Message
+                error
+                header='Action Forbidden'
+                content='You can only sign up for an account once with a given e-mail address.'
+              />
+              <Button>Submit</Button>
+            </Form>
+          </Modal.Content>
+        </Modal>
       </div>
-      </form>
     )
+
+    // return (
+    //   <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+    //   {this.renderAlert()}
+    //   <div className='row'>
+    //     <div className='col-md-6'>
+    //       <label>First Name</label>
+    //       <Field name='firstName' className='form-control' component={renderField} type='text' />
+    //     </div>
+    //     <div className='col-md-6'>
+    //       <label>Last Name</label>
+    //       <Field name='laststName' className='form-control' component={renderField} type='text' />
+    //     </div>
+    //     <div className='col-md-6'>
+    //       <label>User/Email</label>
+    //       <Field name='email' className='form-control' component={renderField} type='text' />
+    //     </div>
+    //     <div className='col-md-6'>
+    //       <label>Password</label>
+    //       <Field name='password' className='form-control' component={renderField} type='text' />
+    //     </div>
+    //     <button type="submit" className="btn btn-primary">Register</button>
+    //   </div>
+    //   </form>
+    // )
   }
 }
 
@@ -88,4 +117,4 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps, { registerUser })(form(Register))
+export default connect(mapStateToProps)(form(Register))
