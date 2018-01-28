7fb0ea486dfa11441813c5c432d42a1b4e29321a
Antonio Rivas <antonio.riva@piksel.com>
2018-01-28 22:25:47 +0100

    Adding reducer registger to index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import totalBenefit from './reducer_total_benefit'
 import authReducer from './reducer_auth'
+import registerReducer from './reducer_register'
 import { coinsUserReducer } from './reducer_coins'
 import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
   totalBenefit,
   auth: authReducer,
+  register: registerReducer,
   form: formReducer,
   coinsUser: coinsUserReducer
 })
