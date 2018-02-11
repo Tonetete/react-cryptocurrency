703600e7003c7e7ccdc6efe5925291fc5c0f7ce9
Antonio Rivas <antonio.riva@piksel.com>
2018-02-11 21:58:34 +0100

    Refactor passport config file to better error messages

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,3 +1,9 @@
+/*
+ To learn more about this section code, visit:
+  http://blog.slatepeak.com/refactoring-a-basic-authenticated-api-with-node-express-and-mongo/
+* */
+
+
 // Importing Passport, strategies, and config
 const passport = require('passport'),  
       User = require('../models/user'),
@@ -6,17 +12,18 @@ const passport = require('passport'),
       ExtractJwt = require('passport-jwt').ExtractJwt,
       LocalStrategy = require('passport-local');
 
+// We tell passport to not use 'username' default field but 'email' instead.
 const localOptions = { usernameField: 'email' };        
 
 // Setting up local login strategy
 const localLogin = new LocalStrategy(localOptions, function(email, password, done) {  
     User.findOne({ email: email }, function(err, user) {
       if(err) { return done(err); }
-      if(!user) { return done(null, false, { error: 'Your login details could not be verified. Please try again.' }); }
+      if(!user) { return done(null, false, { error: 'Your login details could not be verified. User not exists.' }); }
   
       user.comparePassword(password, function(err, isMatch) {
         if (err) { return done(err); }
-        if (!isMatch) { return done(null, false, { error: "Your login details could not be verified. Please try again." }); }
+        if (!isMatch) { return done(null, false, { error: "Your login details could not be verified. Password is incorrect." }); }
   
         return done(null, user);
       });
@@ -44,5 +51,5 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
   });
 
 // allow passport to use the strategies we defined  
-passport.use(jwtLogin);  
-passport.use(localLogin);    
+passport.use(jwtLogin);
+passport.use(localLogin);
