6211bdda4146b7b90eb37f4a55c3206bef4c74a7
Antonio Rivas <antonio.riva@piksel.com>
2018-02-11 21:59:09 +0100

    Cast to obervable when register request epic catch error

--- client/src/epics/register.epic.js
+++ client/src/epics/register.epic.js
@@ -3,6 +3,7 @@ import 'rxjs/add/observable/of'
 import 'rxjs/add/operator/mergeMap'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
+import { Observable } from 'rxjs/Observable'
 
 import { REGISTER_USER_REQUEST } from '../actions/types'
 import { registerUser$, registerUserFail, registerUserSuccess } from '../actions/index'
@@ -17,7 +18,7 @@ export const registerUser = (action$) =>
     .ofType(REGISTER_USER_REQUEST)
     .map(payload => registerUser$(payload.payload))
     .map(payload => registerUserSuccess(payload))
-    .catch(error => registerUserFail(error))
+    .catch(error => Observable.of(registerUserFail(error)))
 // .mergeMap(payload => registerUser$(payload.formFields))
 // .map(payload => registerUserSuccess(payload))
 // .catch(error => registerUserFail(error))
