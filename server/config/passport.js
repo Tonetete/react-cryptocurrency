f5284cb94224e4377ddd1c1688aaa6057b84eaee
Antonio Rivas <antonio.riva@piksel.com>
2018-02-14 20:19:48 +0100

    removing jwtLogin strategy because we're going to use verifyToken cb

--- server/config/passport.js
+++ server/config/passport.js
@@ -30,26 +30,5 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
     });
   });
 
-const jwtOptions = {  
-    // Telling Passport to check authorization headers for JWT
-    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
-    // Telling Passport where to find the secret
-    secretOrKey: config.secret
-};
-
-// Setting up JWT login strategy
-const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {  
-    User.findById(payload._id, function(err, user) {
-      if (err) { return done(err, false); }
-  
-      if (user) {
-        done(null, user);
-      } else {
-        done(null, false);
-      }
-    });
-  });
-
 // allow passport to use the strategies we defined  
-passport.use(jwtLogin);
 passport.use(localLogin);
--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -26,7 +26,6 @@ function setUserInfo(request) {
 // Login Route
 //========================================
 exports.login = function(req, res, next) {
-
     let userInfo = setUserInfo(req.user);
     res.status(200).json({
       token: generateToken(userInfo),
