69cbd30ac4394dffd195faf86097fde3b0314d50
Antonio Rivas <antonio.riva@piksel.com>
2018-07-21 23:31:01 +0200

    Adding prettier and modifying eslint rules to implement this

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,5 +1,38 @@
-const { Neutrino } = require('neutrino');
+module.exports = {
+  "extends": ["plugin:prettier/recommended"],
+  "plugins": ["prettier"],
+  "parser": "babel-eslint",
+  "rules": {
+    "prettier/prettier": "error"
+  }
+};
 
-module.exports = Neutrino()
-  .use('neutrinorc.js')
-  .call('eslintrc');
+
+// const { Neutrino } = require('neutrino');
+
+// module.exports = Neutrino()
+//   .use('neutrinorc.js')
+//   .call('eslintrc');
+
+// module.exports = {
+//   "extends": "eslint:recommended",
+//   "rules": {
+//       // enable additional rules
+//       "indent": ["error", 4],
+//       "linebreak-style": ["error", "unix"],
+//       "quotes": ["error", "double"],
+//       "semi": ["error", "always"],
+
+//       // override default options for rules from base configurations
+//       "comma-dangle": ["error", "always"],
+//       "no-cond-assign": ["error", "always"],
+
+//       // disable rules from base configurations
+//       "no-console": "off",
+//   }
+// }
+
+// module.exports = {
+//   "extends": "airbnb",
+//   "rules": {}
+// };
